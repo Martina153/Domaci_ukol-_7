fc11078faed5010983589321e235e69fec6619dc
Martina_Harsanikova <martinaharsanikova@gmail.com>
2023-03-07 22:46:49 +0100

    hello

--- cenik.js
+++ cenik.js
@@ -2,10 +2,23 @@
 //Tento parametr bude představovat číslo plánu. 
 //Funkce podle čísla plánu vybere ze stránky správný DOM element a přidá k němu CSS třídu plan--selected. 
 //Vyzkoušejte vaši funkci v konzoli s různými čísly.
-  function selectPlan(planNumber) {
+  /*const selectPlan = (planNumber) => {
     // najít prvek na stránce podle planNumber
     const planElement = document.querySelector(`#plan${planNumber}`);
-    
-    // přidat třídu plan--selected
+    // přidat třídu plan--selected  
     planElement.classList.add('plan--selected');
-  }
+  
+    
+  } */
+
+  
+  function selectPlan(planNumber) {
+  const plans = document.querySelectorAll('[id^="plan"]');
+  plans.forEach(plan => {
+    //plan is element
+    if (plan.getAttribute('id') === "plan"+planNumber) {
+      plan.classList.add('plan--selected');
+    } else {
+      plan.classList.remove('plan--selected');
+    }
+  });}
